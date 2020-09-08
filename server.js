f8410d029ddadc1b6bfa622de65737616f4690f2
Glitch (ibrahimerdem-personal) <none>
2020-09-08 14:35:46 +0000

    🐇🏇 Checkpoint ./server.js:365129/157

--- server.js
+++ server.js
@@ -5,9 +5,12 @@ var bodyParser = require("body-parser");
 var getIP = require('ipware')().get_ip;
 var app = express();
 
-mongoose.connect(process.env.MONGO_URI, {useMongoClient: true, useUnifiedTopology: true});
-mongoose.Promise = global.Promise;  
+//mongoose.connect(process.env.MONGO_URI, {useMongoClient: true, useUnifiedTopology: true});
+//mongoose.Promise = global.Promise;  
 
+mongoose.connect(process.env.MONGO_URI).catch(function (reason) {
+    console.log('Unable to connect to the mongodb instance. Error: ', reason);
+});
   
 
 app.use(bodyParser.urlencoded({extended:false}));
